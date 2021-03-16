fefc2b903a66b35d04cce1ba72914ee03013a7db
Dennis Wong <denniswong0106@gmail.com>
2021-03-16 04:22:59 +0000

    added new file for urls_new.ejs, which will eventually take url and shorten into 'shorturl'. Not functional yet, basic template added. express_server.js is updated to load that template

--- express_server.js
+++ express_server.js
@@ -10,19 +10,17 @@ const urlDatabase = {
   "12sM3I": "http://www.youtube.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
-app.get("/hello", (req, res) => {
-  res.send("<html><body>H/b></body></html>\n");
 });
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
