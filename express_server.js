860a31f95f4a2d6373eed1ff761d301c377d668b
Dennis Wong <denniswong0106@gmail.com>
2021-03-16 03:44:53 +0000

    added urls_show.ejs. Added code to urls_index.js to display the short and long urls

--- express_server.js
+++ express_server.js
@@ -3,21 +3,28 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  "12sM3I": "http://www.youtube.com"
 };
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  res.send("<html><body>H/b></body></html>\n");
 });
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
