4e888817b2f5c87f12a54daf51c1e0575f101c14
Dennis Wong <denniswong0106@gmail.com>
2021-03-16 21:29:18 +0000

    added some code for edit function, not yet working

--- express_server.js
+++ express_server.js
@@ -40,7 +40,11 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect(`/urls/`);
 });
-
+// Edit an existing url
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL;
+  res.redirect(`/urls/`);
+});
 // 'get' the generated shortURL page
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
